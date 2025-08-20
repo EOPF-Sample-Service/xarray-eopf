53af27fd4e32d1ed6c8a481c11a380a728c8a19f
konstntokas <konstantin.ntokas@brockmann-consult.de>
2025-08-20 16:38:02 +0200

    tests added

--- integration/test_sen3_native.py
+++ integration/test_sen3_native.py
@@ -7,58 +7,154 @@ from unittest import TestCase
 import xarray as xr
 
 from integration.helpers import assert_dataset_is_chunked
-from xarray_eopf.constants import DEFAULT_ENDPOINT_URL
 from xarray_eopf.utils import timeit
 
 
-s03ol1efr_bucket = "e05ab01a9d56408d82ac32d69a5aae2a:202508-s03olcefr"
-s03ol1err_bucket = "e05ab01a9d56408d82ac32d69a5aae2a:202508-s03olcerr"
-s03ol2lfr_bucket = "e05ab01a9d56408d82ac32d69a5aae2a:202508-s03olclfr"
-s03sl1rbt_bucket = "e05ab01a9d56408d82ac32d69a5aae2a:202508-s03slsrbt"
-s03sl2lst_bucket = "e05ab01a9d56408d82ac32d69a5aae2a:202508-s03slslst"
-path_prefix = "19/products/cpm_v256"
-ol1efr_filename = (
-    "S3B_OL_1_EFR____20250819T074058_20250819T074358_20250819T092155_"
-    "0179_110_106_3420_ESA_O_NR_004.zarr"
-)
-ol1err_filename = (
-    "S3A_OL_1_ERR____20250819T092632_20250819T101045_20250819T113731_"
-    "2653_129_250______PS1_O_NR_004.zarr"
-)
-ol2lfr_filename = (
-    "S3A_OL_2_LFR____20250819T093936_20250819T094236_20250819T114257_"
-    "0180_129_250_2160_PS1_O_NR_003.zarr"
-)
-sl1rbt_filename = (
-    "S3B_SL_1_RBT____20250819T104457_20250819T104757_20250819T124948_"
-    "0180_110_108_2340_ESA_O_NR_004.zarr"
-)
-sl2lst_filenmae = (
-    "S3B_SL_2_LST____20250819T104757_20250819T105057_20250819T130331_"
-    "0179_110_108_2520_ESA_O_NR_004.zarr"
+ol1efr_url = (
+    "https://objects.eodc.eu/e05ab01a9d56408d82ac32d69a5aae2a:202508-s03olcefr/"
+    "19/products/cpm_v256/S3B_OL_1_EFR____20250819T074058_20250819T074358_"
+    "20250819T092155_0179_110_106_3420_ESA_O_NR_004.zarr"
 )
 
+ol1err_url = (
+    "https://objects.eodc.eu/e05ab01a9d56408d82ac32d69a5aae2a:202508-s03olcerr/"
+    "20/products/cpm_v256/S3B_OL_1_ERR____20250820T082222_20250820T090603_"
+    "20250820T113046_2621_110_121______ESA_O_NR_004.zarr"
+)
+
+ol2lfr_url = (
+    "https://objects.eodc.eu/e05ab01a9d56408d82ac32d69a5aae2a:202508-s03olclfr/"
+    "20/products/cpm_v256/S3A_OL_2_LFR____20250820T110024_20250820T110324_"
+    "20250820T125434_0179_129_265_2520_PS1_O_NR_003.zarr"
+)
+sl1rbt_url = (
+    "https://objects.eodc.eu/e05ab01a9d56408d82ac32d69a5aae2a:202508-s03slsrbt/"
+    "20/products/cpm_v256/S3A_SL_1_RBT____20250820T074725_20250820T075025_"
+    "20250820T095144_0180_129_263_3060_PS1_O_NR_004.zarr"
+)
 
 allowed_open_time = 5  # seconds
 
 
-class Sentinel2NativeTest(TestCase):
+class Sentinel3NativeTest(TestCase):
     def test_open_datatree_sen3_ol1efr(self):
-        url = (
-            "https://objects.eodc.eu/e05ab01a9d56408d82ac32d69a5aae2a:202508-s03olcefr/"
-            "19/products/cpm_v256/S3B_OL_1_EFR____20250819T074058_20250819T074358_"
-            "20250819T092155_0179_110_106_3420_ESA_O_NR_004.zarr"
+        self._test_open_datatree_sen3(
+            ol1efr_url, 11, {"columns": 4865, "rows": 4091}, 21
         )
+
+    def test_open_dataset_sen3_ol1efr(self):
+        self._test_open_dataset_sen3(
+            ol1efr_url,
+            "measurements_oa21_radiance",
+            {"measurements_columns": 4865, "measurements_rows": 4091},
+            63,
+        )
+
+    def test_open_dataset_sen3_ol1efr_subgroup(self):
+        self._test_open_dataset_sen3_subgroup(
+            ol1efr_url, "oa21_radiance", {"columns": 4865, "rows": 4091}, 21
+        )
+
+    def test_open_datatree_sen3_ol1err(self):
+        self._test_open_datatree_sen3(
+            ol1err_url, 8, {"columns": 1217, "rows": 14893}, 21
+        )
+
+    def test_open_dataset_sen3_ol1err(self):
+        self._test_open_dataset_sen3(
+            ol1err_url,
+            "measurements_oa21_radiance",
+            {"measurements_columns": 1217, "measurements_rows": 14893},
+            59,
+        )
+
+    def test_open_dataset_sen3_ol1err_subgroup(self):
+        self._test_open_dataset_sen3_subgroup(
+            ol1err_url, "oa21_radiance", {"columns": 1217, "rows": 14893}, 21
+        )
+
+    def test_open_datatree_sen3_ol2lfr(self):
+        self._test_open_datatree_sen3(ol2lfr_url, 9, {"columns": 4865, "rows": 4091}, 5)
+
+    def test_open_dataset_sen3_ol2lfr(self):
+        self._test_open_dataset_sen3(
+            ol2lfr_url,
+            "measurements_otci",
+            {"measurements_columns": 4865, "measurements_rows": 4091},
+            28,
+        )
+
+    def test_open_dataset_sen3_ol2lfr_subgroup(self):
+        self._test_open_dataset_sen3_subgroup(
+            ol2lfr_url, "otci", {"columns": 4865, "rows": 4091}, 5
+        )
+
+    def test_open_datatree_sen3_sl1rbt(self):
+        self._test_open_datatree_sen3(
+            sl1rbt_url, 97, {"columns": 4865, "rows": 4091}, 97
+        )
+
+    def test_open_dataset_sen3_sl1rbt(self):
+        self._test_open_dataset_sen3(
+            sl1rbt_url,
+            "measurements_bnadir_s4_radiance_bn",
+            {"measurements_bnadir_columns": 3000, "measurements_bnadir_rows": 2400},
+            739,
+        )
+
+    def test_open_dataset_sen3_sl1rbt_subgroup(self):
+        self._test_open_dataset_sen3_subgroup(
+            sl1rbt_url,
+            "bnadir_s4_radiance_bn",
+            {"anadir_columns": 3000, "anadir_rows": 2400},
+            39,
+        )
+
+    def _test_open_datatree_sen3(
+        self, url: str, num_groups: int, ds_sizes: dict, num_data_vars: int
+    ):
         with timeit("open " + url) as result:
             # noinspection PyTypeChecker
             dt = xr.open_datatree(url, engine="eopf-zarr", op_mode="native", chunks={})
         self.assertTrue(result.time_delta < allowed_open_time)
-        self.assertEqual(11, len(dt.groups))
+        self.assertEqual(num_groups, len(dt.groups))
         self.assertIn(
             "/measurements",
             dt.groups,
         )
         ds = dt.measurements
-        self.assertEqual({"columns": 4865, "rows": 4091}, ds.sizes)
-        self.assertEqual(21, len(ds.data_vars))
+        self.assertEqual(ds_sizes, ds.sizes)
+        self.assertEqual(num_data_vars, len(ds.data_vars))
+        assert_dataset_is_chunked(self, ds, verbose=True)
+
+    def _test_open_dataset_sen3(
+        self, url: str, name_data_var: str, ds_sizes: dict, num_data_vars: int
+    ):
+        with timeit("open " + url) as result:
+            # noinspection PyTypeChecker
+            ds = xr.open_dataset(url, engine="eopf-zarr", op_mode="native", chunks={})
+        self.assertTrue(result.time_delta < allowed_open_time)
+        self.assertEqual(num_data_vars, len(ds.data_vars))
+        self.assertIn(name_data_var, ds.data_vars)
+        self.assertEqual(
+            ds_sizes,
+            ds[name_data_var].sizes,
+        )
+        assert_dataset_is_chunked(self, ds, verbose=True)
+
+    def _test_open_dataset_sen3_subgroup(
+        self, url: str, name_data_var: str, ds_sizes: dict, num_data_vars: int
+    ):
+        with timeit("open " + f"{url}/measurements") as result:
+            # noinspection PyTypeChecker
+            ds = xr.open_dataset(
+                f"{url}/measurements",
+                engine="eopf-zarr",
+                op_mode="native",
+                chunks={},
+            )
+        self.assertTrue(result.time_delta < allowed_open_time)
+        self.assertEqual(num_data_vars, len(ds.data_vars))
+        self.assertIn(name_data_var, ds.data_vars)
+        self.assertEqual(ds_sizes, ds.sizes)
         assert_dataset_is_chunked(self, ds, verbose=True)

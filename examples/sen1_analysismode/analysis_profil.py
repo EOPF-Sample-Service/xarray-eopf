8a415adc92daacee37065a2f6eea93a06ddd9700
konstntokas <konstantin.ntokas@brockmann-consult.de>
2026-04-14 15:39:46 +0200

    backup, gtc and rtc work

--- examples/sen1_analysismode/analysis_profil.py
+++ examples/sen1_analysismode/analysis_profil.py
@@ -1,10 +1,10 @@
-mport os
+import os
 import pstats
 from pathlib import Path
 
 # Load the saved profiling data
 DIR = Path(__file__).parent.resolve()
-stats = pstats.Stats(os.path.join(DIR, "debug_resampling_sen3_new.prof"))
+stats = pstats.Stats(os.path.join(DIR, "gtc.prof"))
 
 # Sort by cumulative time and display the top 100 entries
 stats.strip_dirs().sort_stats("cumtime").print_stats(100)
